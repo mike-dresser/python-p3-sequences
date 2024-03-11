d6d7859030b518dd2a560076380ed88fd125020f
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-11 13:24:58 -0400

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    result = [0, 1, 1]
+    while len(result) < length:
+        result.append(result[-2] + result[-1])
+    print([] if length == 0 else result[0:length] if length < 3 else result)
+    
